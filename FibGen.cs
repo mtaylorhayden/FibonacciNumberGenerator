277a9e657f9ba76cd70bd65ec965dbbce9a9b058
mtaylorhayden <44408748+mtaylorhayden@users.noreply.github.com>
2019-07-05 16:49:26 -0500

    Add files via upload

---
+++ FibGen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fibGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            List<int> fibList = fibGen(n);
+
+            fibList.ForEach(Console.WriteLine);
+
+        }
+
+        private static List<int> fibGen(int n)
+        {
+            List<int> fibs = new List<int>();
+
+            int i = 0;
+            int j = 1;
+
+            while (fibs.Count < n)
+            {
+
+                int place = j;
+
+                int temp = i + j;
+
+                fibs.Add(temp);
+
+                i = place;
+                j = temp;
+
+            }
+            return fibs;
+        }
+    }
+}
